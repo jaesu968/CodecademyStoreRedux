2a487b381076ea0ea4b1cb2ed41c96ecdd1c60ce
jaesu968 <mazterk968@gmail.com>
2025-12-01 13:08:59 -0600

    store update2 imports

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,5 +2,8 @@
 import { createStore, combineReducers } from 'redux'; 
 
 // Import the slice reducers here.
+import { inventoryReducer } from '../features/inventory/inventorySlice'; 
+import { cartReducer } from '../features/cart/cartSlice';
+import { currencyFilterReducer } from '../features/currencyFilter/currencyFilterSlice';
 
 // Create and export the store here.
